d850939edeffeb56eadacbbbedb7f2b50dbb836f
Theresa <tmannschatz@gmail.com>
2020-09-26 14:16:18 +0200

    Copy devise views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.force_ssl = false
 
   # Show full error reports.
   config.consider_all_requests_local = true
