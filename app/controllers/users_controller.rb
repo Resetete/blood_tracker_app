36799389c2419f82936004e1c8ed20d36f2f415f
Theresa <tmannschatz@gmail.com>
2020-09-27 22:36:30 +0200

    Restrict access to view account pages if not of current_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
+  before_action :require_same_user
+  before_action :set_user, only: [:show]
 
   def show
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user.id.to_s != params[:id]
+      flash[:alert] = 'You can only view your own profile'
+      redirect_to root_path
+    end
   end
 end
