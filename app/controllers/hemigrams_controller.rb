ec2a9beace1af85dcb30625db6fef3c00794137a
Theresa <tmannschatz@gmail.com>
2020-09-27 23:36:12 +0200

    Add hemigram new and create action, set up draft form

--- app/controllers/hemigrams_controller.rb
+++ app/controllers/hemigrams_controller.rb
@@ -3,4 +3,27 @@ class HemigramsController < ApplicationController
   def index
     @all_user_hemigrams = current_user.hemigrams
   end
+
+  def new
+    @hemigram = Hemigram.new
+  end
+
+  def create
+    @hemigram = Hemigram.new(hemigram_params)
+    @hemigram.user_id = current_user.id
+    
+    if @hemigram.save
+      flash[:notice] = 'You successfully saved your hemigram'
+      redirect_to new_hemigram_path
+    else
+      flash[:alert] = 'Something went wrong, please try again'
+      redirect_to new_hemigram_path
+    end
+  end
+
+  private
+
+  def hemigram_params
+    params.require(:hemigram).permit(:parameter, :value)
+  end
 end
