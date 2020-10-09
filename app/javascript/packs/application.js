c69c87ccc3936ae2fe702133674a18d6fd5b49e1
Theresa <tmannschatz@gmail.com>
2020-10-09 14:23:13 +0200

    Remove bootstrap, add custom css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('bootstrap')
 
+// import custom stylesheets from javascript/stylesheets/
+require("../stylesheets/custom.css.scss")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
