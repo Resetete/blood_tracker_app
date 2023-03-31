cead12a5a935cdcf0d9c60aded82e48f4a6c3b7e
Theresa <tmannschatz@gmail.com>
2023-03-31 21:16:19 +0200

    Upgrade to rails 7 and ruby 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_04_120529) do
+ActiveRecord::Schema[6.1].define(version: 2020_10_04_120529) do
 
   create_table "hemigrams", force: :cascade do |t|
     t.string "parameter"
