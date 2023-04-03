b70446454294443bdb5f5697ee7aa7cada1e5b27
Theresa <tmannschatz@gmail.com>
2023-04-03 23:20:45 +0200

    Finalize migration to importmap; Rails7; add bootstrap

---
+++ app/javascript/application.js
@@ -0,0 +1,17 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+import "@hotwired/turbo-rails"
+import './src/jquery'
+import 'controllers'
+import * as bootstrap from "bootstrap"
+
+// $(document).on('turbo:load', function() {
+//   $('select').formSelect();
+//   $(".dropdown-trigger").dropdown();
+//   $("#fade-out-target").fadeOut(4000);
+//   $('.datepicker').datepicker({
+//     format:'yyyy-mm-dd',
+//     firstDay: 1,
+//   });
+// });
+
