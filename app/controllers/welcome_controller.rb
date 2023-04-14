4142ac073a6129984f7854e65b5c3b8117239779
Theresa <tmannschatz@gmail.com>
2023-04-14 23:40:50 +0200

    Add connection to medlineplus API

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,9 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    @thrombocytes_article = Encyclopedia::Article.new('platelet disorders')
+    @lymphocyte_article = Encyclopedia::Article.new('lymphocyte')
+
+
   end
 end
---
+++ app/models/encyclopedia/article.rb
@@ -0,0 +1,63 @@
+require 'faraday'
+
+module Encyclopedia
+  class Article
+    # returns entries from https://medlineplus.gov/
+    BASE_URL = 'https://wsearch.nlm.nih.gov/ws'.freeze
+
+    attr_reader :article, :url, :source, :summary, :title
+
+    def initialize(search_term)
+      article = first_article_for(search_term)
+
+      @article = article
+      @url = url_of(article)
+      @title = title_of(article)
+      @source = source_of(article)
+      @summary = summary_of(article)
+    end
+
+    def first_article_for(search_term)
+      articles = look_up(search_term)
+      articles.dig(:nlmSearchResult,:list).second.dig(:document)
+    end
+
+    def look_up(search_term)
+      response = client.get('query', { db: 'healthTopics', term: search_term })
+      xml_as_hash(response.body)
+    end
+
+    private
+
+    def url_of(article)
+      article.first.dig(:url)
+    end
+
+    def title_of(article)
+      parse_article_document(article).find{ |hash| hash['title'] }.values.join
+    end
+
+    def source_of(article)
+      parse_article_document(article).find{ |hash| hash['organizationName'] }.values.join
+    end
+
+    def summary_of(article)
+      parse_article_document(article).find{ |hash| hash['FullSummary'] }.values.join
+    end
+
+    def parse_article_document(article)
+      article.flat_map(&:values).map{|elem| {(elem.is_a?(String) ? elem : elem.first.values.join) => (elem.is_a?(String) ? nil : elem.second)}}
+    end
+
+    def client
+      Faraday.new(
+        url: BASE_URL,
+        headers: { 'Content-Type' => 'application/xml' },
+      )
+    end
+
+    def xml_as_hash(xml_body)
+      Ox.load(xml_body, mode: :hash)
+    end
+  end
+end
