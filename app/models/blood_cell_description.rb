c3a57338e4b5fc360981ff4fb99225a035f50a43
Theresa <tmannschatz@gmail.com>
2023-05-12 15:52:09 +0200

    Use cocoon for nested link fields

--- app/models/blood_cell_description.rb
+++ app/models/blood_cell_description.rb
@@ -16,5 +16,5 @@
 # this allows storing and editing of the description of blood cells
 class BloodCellDescription < ApplicationRecord
   has_many :links, dependent: :destroy
-  accepts_nested_attributes_for :links
+  accepts_nested_attributes_for :links, allow_destroy: true, reject_if: :all_blank
 end
