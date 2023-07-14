524a57892ca876a741a2022f153f0419d176aea9
Theresa <tmannschatz@gmail.com>
2023-07-14 23:22:17 +0200

    rework main section view for small devices

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -10,7 +10,9 @@ import "@nathanvda/cocoon"
 
 $(document).on('turbo:load', function() {
   // resizes the navbar logo and brand when scrolling down 80px from top
-  window.onscroll = function() {scrollFunction()};
+  if (window.innerWidth >= 768){
+    window.onscroll = function() {scrollFunction()};
+  }
 
   function scrollFunction() {
     if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
