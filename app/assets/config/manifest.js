7b7574bbbf52806d7f87c5bf41d952d85efbc5fe
Theresa <tmannschatz@gmail.com>
2023-07-14 20:48:56 +0200

    Add favicons to link_tree manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,5 @@
 //= link_tree ../images
+//= link_tree ../favicons
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link apple-touch-icon.png
