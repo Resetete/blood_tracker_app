34581ddb03ea44274403c984f04cc2f38ca6fa56
Theresa <tmannschatz@gmail.com>
2023-11-17 17:39:04 +0100

    Clean views, remove recoverable from user, generate recovery codes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,39 +4,31 @@
 #
 # Table name: users
 #
-#  id                     :bigint           not null, primary key
-#  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
-#  reset_password_token   :string
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  confirmation_token     :string
-#  confirmed_at           :datetime
-#  confirmation_sent_at   :datetime
-#  unconfirmed_email      :string
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#  admin                  :boolean
-#  username               :string           default(""), not null
+#  id                  :bigint           not null, primary key
+#  encrypted_password  :string           default(""), not null
+#  remember_created_at :datetime
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  admin               :boolean
+#  username            :string           default(""), not null
+#  recovery_codes      :string           default([]), not null, is an Array
+#  security_questions  :string           default([]), is an Array
 #
 class User < ApplicationRecord
   has_many :hemigrams, dependent: :destroy
 
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :validatable
 
-  # Registerable temporarly desactivated
-  # TODO: allow registration when tracker is ready
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
   has_one :chart_setting, class_name: 'Hemigrams::ChartSetting'
 
-  validates :recovery_codes, presence: true
   validates :security_questions, presence: true
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: { minimum: 8 }
 
-  before_save :generate_recovery_codes
+  after_create :generate_recovery_codes
 
   def admin?
     admin
@@ -45,6 +37,6 @@ class User < ApplicationRecord
   private
 
   def generate_recovery_codes
-
+    self.recovery_codes = 5.times.map { SecureRandom.alphanumeric(8) }
   end
 end
