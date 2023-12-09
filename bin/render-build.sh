0eea169e724dfe8af0e34de581e742e2a3a73848
Theresa <tmannschatz@gmail.com>
2023-12-09 23:41:06 +0100

    Install charts plugin with yarn

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,8 +4,11 @@
 set -o errexit
 
 bundle install
+# install packages
+yarn add chartjs-plugin-annotation
+# clean assets
 bundle exec bin/rails assets:precompile
 bundle exec bin/rails assets:clean
-bundle exec bin/rails db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+# bundle exec bin/rails db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec bin/rails db:migrate
 bundle exec bin/rails db:seed
