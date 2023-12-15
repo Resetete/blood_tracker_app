71c590aac50e001e8315fcf5c8a67aa9a1163085
Theresa <tmannschatz@gmail.com>
2023-12-15 21:51:23 +0100

    Add ParameterMetadata to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,11 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 # Or run Rails.application.load_seed in rails console
 
-# Create an admin user
+def find_parameter(parameter_name)
+  Admin::Hemigrams::ParameterMetadata.find_by(parameter_name:)
+end
+
+# Create an admin user it it doesn't exist
 if User.find_by(username: Rails.application.credentials.admin.username)
   Rails.logger.debug 'Admin user already exists'
 else
@@ -1118,4 +1122,37 @@ else
   blood_glucose_levels.save
 
   Rails.logger.debug 'Blood Glucose Levels description created'
+end
+
+# Blood metadata
+[
+  { :parameter_name=>"thrombozythes", :abbreviations=>["PLT", "thrombos"], :chart_unit=>"10^3/µL", :upper_limit=>450.0, :lower_limit=>150.0},
+  { :parameter_name=>"hemoglobin", :abbreviations=>["Hb", "Hgb"], :chart_unit=>"g/dL", :upper_limit=>18.0, :lower_limit=>12.0},
+  { :parameter_name=>"hematocrit", :abbreviations=>["Hct"], :chart_unit=>"%", :upper_limit=>52.0, :lower_limit=>37.0},
+  { :parameter_name=>"red_blood_cells", :abbreviations=>["RBC", "erythrocytes"], :chart_unit=>"10^6/μL", :upper_limit=>6.0, :lower_limit=>4.5},
+  { :parameter_name=>"white_blood_cells", :abbreviations=>["WBC", "leukocytes"], :chart_unit=>"10^6/μL", :upper_limit=>11.0, :lower_limit=>4.0},
+  { :parameter_name=>"mean_corpuscular_hemoglobin", :abbreviations=>["MCH"], :chart_unit=>"pg", :upper_limit=>34.0, :lower_limit=>27.0},
+  { :parameter_name=>"mean_corpuscular_hemoglobin_concentration", :abbreviations=>["MCHC"], :chart_unit=>"g/dl", :upper_limit=>36.0, :lower_limit=>32.0},
+  { :parameter_name=>"red_cell_distribution_width", :abbreviations=>["RDW"], :chart_unit=>"%", :upper_limit=>16.0, :lower_limit=>11.0},
+  { :parameter_name=>"prothrombin_time", :abbreviations=>["PT"], :chart_unit=>"seconds", :upper_limit=>15.0, :lower_limit=>11.0},
+  { :parameter_name=>"fibrinogen", :abbreviations=>["-"], :chart_unit=>"mg/dL", :upper_limit=>400.0, :lower_limit=>200.0},
+  { :parameter_name=>"neutrophils", :abbreviations=>["NEUT", "Neutro"], :chart_unit=>"%", :upper_limit=>70.0, :lower_limit=>40.0},
+  { :parameter_name=>"lymphocytes", :abbreviations=>["Lymph"], :chart_unit=>"%", :upper_limit=>40.0, :lower_limit=>20.0},
+  { :parameter_name=>"monocytes", :abbreviations=>["Mono"], :chart_unit=>"%", :upper_limit=>10.0, :lower_limit=>2.0},
+  { :parameter_name=>"eosinophils", :abbreviations=>["EO"], :chart_unit=>"%", :upper_limit=>5.0, :lower_limit=>1.0},
+  { :parameter_name=>"basophils", :abbreviations=>["BASO"], :chart_unit=>"%", :upper_limit=>2.0, :lower_limit=>0.5},
+  { :parameter_name=>"mean_corpuscular_volume", :abbreviations=>["MCV"], :chart_unit=>"fL", :upper_limit=>100.0, :lower_limit=>80.0},
+  { :parameter_name=>"platelet_distribution_width", :abbreviations=>["PDW"], :chart_unit=>"fL", :upper_limit=>20.0, :lower_limit=>9.0},
+  { :parameter_name=>"mean_platelet_volume", :abbreviations=>["MPV"], :chart_unit=>"fL", :upper_limit=>13.0, :lower_limit=>7.0}
+].each do |parameter_attr|
+  parameter = find_parameter(parameter_attr[:parameter_name])
+  return Rails.logger.debug("ParameterMetadata #{parameter.parameter_name} already exists") if parameter.present?
+
+  Admin::Hemigrams::ParameterMetadata.create(
+    parameter_name: parameter.parameter_name,
+    abbreviations: parameter.abbreviations,
+    chart_unit: parameter.chart_unit,
+    upper_limit: parameter.upper_limit,
+    lower_limit: parameter.lower_limit,
+  )
 end
