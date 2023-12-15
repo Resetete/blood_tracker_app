ab1999eec3ff8103d5e3485f01904a15b4ca3de9
Theresa <tmannschatz@gmail.com>
2023-12-15 11:25:20 +0100

    Add some echo printing to renderer yml

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,10 +5,14 @@ set -o errexit
 
 bundle install
 # install packages
+echo install chartjs-plugin-annotation
 yarn add chartjs-plugin-annotation
 # clean assets
+echo precompile and clean assets
 bundle exec bin/rails assets:precompile
 bundle exec bin/rails assets:clean
 # bundle exec bin/rails db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+echo run db:migrate
 bundle exec bin/rails db:migrate
+echo run db:seed
 bundle exec bin/rails db:seed
