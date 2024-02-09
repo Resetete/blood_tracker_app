e067beb0af06f29ba18e566d3c8e4802499e51ca
Theresa <tmannschatz@gmail.com>
2024-02-09 18:47:48 +0100

    Add status check for migrations

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Set the environment to production
+export RAILS_ENV=production
+
 # exit on error
 set -o errexit
 set -o pipefail
@@ -9,18 +12,21 @@ echo "Install packages"
 yarn install
 bundle install
 
+# run setup script
+# echo "Run setup script"
+# only for first deployment
+# bin/setup
+
 # clean and precompile assets
 echo "Precompile assets"
 bundle exec bin/rails assets:precompile
 bundle exec bin/rails assets:clean
 
+# check status of migration
+bin/rails db:migrate:status
+bin/rails db:migrate
+
 # seed the db
 bundle exec bin/rails db:seed
-
-# run setup script
-echo "Run setup script"
-bin/setup
-
-bin/rails db:migrate
 
 echo "Build script completed successfully"
