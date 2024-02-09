f0ec3f1b4d8c0992b6dc9b174ea2919301eb0feb
Theresa <tmannschatz@gmail.com>
2024-02-09 15:10:38 +0100

    Add sparkling sparks animation to landing page + imprint reference

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -16,6 +16,121 @@ import { Chart, registerables } from "chart.js"
 Chart.register(...registerables)
 import 'chartjs-plugin-annotation';
 
+// animation on landing page
+function initializeAnimation() {
+  var canvas, ctx, mouse, gravityStrength, particles, spawnTimer, spawnInterval, type, time, particleOverflow, x, y, a;
+
+  window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+  var init = function () {
+    canvas = document.querySelector('canvas');
+    ctx = canvas.getContext('2d');
+
+    var onresize = function () {
+      canvas.width = canvas.clientWidth;
+      canvas.height = canvas.clientHeight;
+    };
+    onresize();
+
+    mouse = { x: canvas.width / 2, y: canvas.height / 2, out: false };
+
+    canvas.onmouseout = function () {
+      mouse.out = true;
+    };
+
+    canvas.onmousemove = function (e) {
+      var rect = canvas.getBoundingClientRect();
+      mouse = {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top,
+        out: false
+      };
+    };
+
+    gravityStrength = 10;
+    particles = [];
+    spawnTimer = 0;
+    spawnInterval = 10;
+    type = 0;
+    requestAnimationFrame(startLoop);
+  };
+
+  var newParticle = function () {
+    type = type ? 0 : 1;
+    particles.push({
+      x: mouse.x,
+      y: mouse.y,
+      xv: type ? 18 * Math.random() - 9 : 24 * Math.random() - 12,
+      yv: type ? 18 * Math.random() - 9 : 24 * Math.random() - 12,
+      c: type ? 'rgb(255,' + ((200 * Math.random()) | 0) + ',' + ((80 * Math.random()) | 0) + ')' : 'rgb(255,255,255)',
+      s: type ? 5 + 10 * Math.random() : 1,
+      a: 1
+    });
+  };
+
+  var startLoop = function (newTime) {
+    time = newTime;
+    requestAnimationFrame(loop);
+  };
+
+  var loop = function (newTime) {
+    draw();
+    calculate(newTime);
+    requestAnimationFrame(loop);
+  };
+
+  var draw = function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (var i = 0; i < particles.length; i++) {
+      var p = particles[i];
+      ctx.globalAlpha = p.a;
+      ctx.fillStyle = p.c;
+      ctx.beginPath();
+      ctx.arc(p.x, p.y, p.s, 0, 2 * Math.PI);
+      ctx.fill();
+    }
+  };
+
+  var calculate = function (newTime) {
+    var dt = newTime - time;
+    time = newTime;
+
+    if (!mouse.out) {
+      spawnTimer += dt < 100 ? dt : 100;
+      for (; spawnTimer > 0; spawnTimer -= spawnInterval) {
+        newParticle();
+      }
+    }
+
+    particleOverflow = particles.length - 700;
+    if (particleOverflow > 0) {
+      particles.splice(0, particleOverflow);
+    }
+
+    for (var i = 0; i < particles.length; i++) {
+      var p = particles[i];
+      if (!mouse.out) {
+        x = mouse.x - p.x;
+        y = mouse.y - p.y;
+        a = x * x + y * y;
+        a = a > 100 ? gravityStrength / a : gravityStrength / 100;
+        p.xv = (p.xv + a * x) * 0.99;
+        p.yv = (p.yv + a * y) * 0.99;
+      }
+      p.x += p.xv;
+      p.y += p.yv;
+      p.a *= 0.99;
+    }
+  };
+
+  var onload = function () {
+    setTimeout(init, 0);
+  };
+
+  // Initial setup
+  onload();
+};
+
 // updates the search term in the url when searching with turbo frame in mediline
 document.addEventListener("turbo:submit-end", function (event) {
   event.preventDefault();
@@ -127,5 +242,6 @@ $(document).on('turbo:load', function() {
     localStorage.setItem('cookieSeen','shown')
     $('.cookie-banner').fadeOut();
   });
-});
 
+  initializeAnimation();
+});
