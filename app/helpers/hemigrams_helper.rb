a2a809648901efcfab9ffd84bca3b04330e40a24
Theresa <tmannschatz@gmail.com>
2024-05-09 18:14:48 +0200

    Add some variations of encouraging phrases to track

--- app/helpers/hemigrams_helper.rb
+++ app/helpers/hemigrams_helper.rb
@@ -28,4 +28,31 @@ module HemigramsHelper
       unit
     end
   end
+
+  def encouraging_phrase(number_hemigrams)
+    if number_hemigrams.between?(0,10)
+      [
+        'You are off to a great start! Keep adding more hemigram data to track your progress.',
+        'Nice work! Continue adding hemigram data to see how your health progresses over time.',
+        'You are doing great! Keep going and add more hemigram data to get a comprehensive view of your health.',
+        'Well done! Add more hemigram data to build a complete picture of your health journey.',
+        'Off to a great start! Keep adding data to build a comprehensive picture of your health.',
+        'You are on your way! Keep adding more data to unlock deeper insights into your health.',
+      ].sample(1).join
+    elsif number_hemigrams.between?(11,40)
+      [
+        "Making strides! Keep adding data to enrich your health insights.",
+        "You are on the right track! Keep adding entries to deepen your understanding of your health.",
+        "Nice work! Keep up the momentum by adding more entries to your hemigram.",
+        "You are doing great! Keep adding data to maintain your progress and improve your health insights.",
+        "Progressing well! Keep adding entries to your hemigram to continue uncovering valuable insights.",
+      ].sample(1).join
+    else
+      [
+        'Keep up the great work!',
+        "Impressive progress! Your dedication to tracking your health is paying off. Keep it up!",
+        "Fantastic job! With so much data, you're gaining valuable insights into your health. Keep going!",
+      ].sample(1).join
+    end
+  end
 end
