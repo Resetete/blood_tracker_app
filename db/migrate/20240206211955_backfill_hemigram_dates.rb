6d3e32e10b5b7d2c3df5293ddc7f002a2b167620
Theresa <tmannschatz@gmail.com>
2024-05-10 22:07:09 +0200

    Migration

--- db/migrate/20240206211955_backfill_hemigram_dates.rb
+++ db/migrate/20240206211955_backfill_hemigram_dates.rb
@@ -5,7 +5,7 @@ class BackfillHemigramDates < ActiveRecord::Migration[7.0]
   def change
     Hemigram.distinct.pluck(:date).each do |date|
       # Find or create a Hemigrams::Date record for the current date
-      hemigram_date = Hemigrams::Date.find_or_create_by(date:)
+      hemigram_date = Hemigrams::Date.find_or_create_by(date: date.to_date)
 
       # Update all hemigrams with the id of the Hemigrams::Date object
       Hemigram.where(date:).update_all(record_date_id: hemigram_date.id)
