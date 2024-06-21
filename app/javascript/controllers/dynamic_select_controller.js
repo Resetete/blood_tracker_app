444e5839f48bb8884b703cde5bcc35fa4e623dce
Theresa <tmannschatz@gmail.com>
2024-06-21 17:00:15 +0200

    Fix unit selection dropdown in edit form

--- app/javascript/controllers/dynamic_select_controller.js
+++ app/javascript/controllers/dynamic_select_controller.js
@@ -7,18 +7,24 @@ export default class extends Controller {
     this.element.dataset.action = "change->dynamic-select#change"
   }
 
+  // New and edit form
   // when there is already a value selected (not 'Select Parameter')
   // and we listen to the change of the second dropdown and show the unit options
   // each form needs dynamic dropdown select ids so that the javascript works specifically for each dropdown
 
   change() {
     // Check if the first dropdown has a selected value
-    const objectId = this.element.dataset.objectId
-    console.log(objectId);
-    const parameterSelector = 'parameter-select-' + objectId;
-    console.log(parameterSelector);
-    const firstDropdown = document.getElementById(parameterSelector);
-    const firstDropdownValue = firstDropdown?.value;
+    const objectId = this.element.dataset.objectId || this.context.parentElement.id.split('-').pop()
+
+    let firstDropdownValue;
+    let firstDropdown;
+
+    if (objectId) {
+      const parameterSelector = 'parameter-select-' + objectId;
+      firstDropdown = document.getElementById(parameterSelector);
+      // If possible get the value of the first Parameter select dropdown
+      firstDropdownValue = firstDropdown?.value;
+    }
 
     if (!firstDropdownValue) {
       // Insert an error message below the second dropdown
@@ -38,6 +44,7 @@ export default class extends Controller {
     const url = this.element.dataset.url
     // fetch turbo-frame from the select
     const turboFrameId = this.element.dataset.turboFrame + '-' + objectId
+    console.log('this.element.dataset.turboFrame', this.element.dataset.turboFrame);
     console.log("turboFrame", turboFrameId);
 
     // create new url with the value
@@ -47,8 +54,12 @@ export default class extends Controller {
     // replace the turbo-frame with the new url
     // for the new create form
     let frame = document.querySelector(`turbo-frame#${turboFrameId}`)
+    console.log('frame', frame);
 
-    frame.src = this.url
-    frame.reload()
+    // We do not reload the frame in the edit form
+    // if (frame) {
+      frame.src = this.url
+      frame.reload()
+    // }
   }
 }
