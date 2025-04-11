3c090cfe93e647715c37bd053833ae181780ef99
Theresa <tmannschatz@gmail.com>
2025-04-11 13:47:42 +0200

    Fix typo

--- db/migrate/20250411093224_change_recovery_codes_and_security_questions_to_jsonb.rb
+++ db/migrate/20250411093224_change_recovery_codes_and_security_questions_to_jsonb.rb
@@ -1,30 +1,28 @@
 # this is needed to be able to encrypt those fields through the encrypts model method
 class ChangeRecoveryCodesAndSecurityQuestionsToJsonb < ActiveRecord::Migration[7.0]
   def up
-    # 1. Remove default (not just change it!)
+    # 1. Clear the data in these columns
+    User.update_all(recovery_codes: [], security_questions: [])
+
+    # 2. Remove default (not just change it!)
     change_column_default :users, :recovery_codes, nil
     change_column_default :users, :security_questions, nil
 
-    # 2. Use raw SQL to convert columns from array to jsonb
-    execute <<-SQL
-      ALTER TABLE users
-      ALTER COLUMN recovery_codes TYPE jsonb USING recovery_codes::jsonb;
-      ALTER COLUMN security_questions TYPE jsonb USING security_questions::jsonb;
-    SQL
+    # 3. Convert to jsonb
+    change_column :users, :recovery_codes, :jsonb, null: false, default: []
+    change_column :users, :security_questions, :jsonb, null: false, default: []
 
-    # 3. Re-add proper defaults
+    # 4. Re-add proper defaults
     change_column_default :users, :recovery_codes, from: nil, to: []
     change_column_default :users, :security_questions, from: nil, to: []
   end
 
   def down
     # Revert the changes if needed
-    execute <<-SQL
-      ALTER TABLE users
-      ALTER COLUMN recovery_codes TYPE text[] USING recovery_codes::text[];
-      ALTER COLUMN security_questions TYPE text[] USING security_questions::text[];
-    SQL
+    change_column :users, :recovery_codes, :text, null: false, default: []
+    change_column :users, :security_questions, :text, null: false, default: []
 
     change_column_default :users, :recovery_codes, from: [], to: nil
     change_column_default :users, :security_questions, from: [], to: nil
   end
+end
